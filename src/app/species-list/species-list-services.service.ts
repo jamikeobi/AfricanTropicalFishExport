9b77fc062647ade8ad4cd9ab973841c55e53bb49
Jamike <jamikeobikwelu@gmail.com>
2024-07-11 13:57:00 +0100

    Thurday's class change

--- src/app/species-list/species-list-services.service.ts
+++ src/app/species-list/species-list-services.service.ts
@@ -7,15 +7,15 @@ import { Observable, of, BehaviorSubject , map} from 'rxjs';
 export class SpeciesListServicesService {
 
   private CongoFishList = [
-    {id: 1, Fishname: 'ABC', source: '../../assets/img/CongoFishes/1.mp4'},
+    {id: 1, Fishname: 'Hydrocynus goliath', source: '../../assets/img/CongoFishes/1.mp4'},
     {id: 2, Fishname: 'MBU PUFFER (Tetradon Mbu) ', source: "../../assets/img/CongoFishes/video3.mp4"},
     {id: 3, Fishname: 'MIRUS PUFFER (Tetradon Mirus)', source: "../../assets/img/CongoFishes/3.mp4"},
     {id: 4, Fishname: 'POLYPTERUS (Polypterus Palmas) ', source: "../../assets/img/CongoFishes/4.mp4"},
     {id: 5, Fishname: 'ORNATIPINNIS (Polypterus Ornatipinnis) ', source: "../../assets/img/CongoFishes/5.mp4"},
-    {id: 6, Fishname: 'ABC', source: "../../assets/img/CongoFishes/6.mp4"},
-    {id: 7, Fishname: 'ABC', source: "../../assets/img/CongoFishes/7.mp4"},
-    // {id: 8, Fishname: 'ABC', source: "../../assets/img/CongoFishes/8.mp4"},
-    // {id: 9, Fishname: 'ABC', source: "../../assets/img/CongoFishes/9.mp4"},
+    {id: 6, Fishname: ' Polypterus Mokelembembe', source: "../../assets/img/CongoFishes/6.mp4"},
+    {id: 7, Fishname: 'Phenaco aurantiacus', source: "../../assets/img/CongoFishes/7.mp4"},
+    {id: 8, Fishname: 'Protopterus dolloi albinos', source: "../../assets/img/CongoFishes/14.mp4"},
+    {id: 9, Fishname: 'Distichodus sexfaciastis', source: "../../assets/img/CongoFishes/12.mp4"},
 
   ];
 
